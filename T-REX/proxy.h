edc3c1f45d1a0b0a9fefb7019ecffe70fe9e12e0
Parham Alvani <parham.alvani@gmial.com>
2015-03-23 02:13:23 +0430

    proxy object is added.

---
+++ T-REX/proxy.h
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : proxy.h
+ *
+ * [] Creation Date : 23-03-2015
+ *
+ * [] Last Modified : Mon 23 Mar 2015 02:12:06 AM IRDT
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef PROXY_H
+#define PROXY_H
+
+void trex_init(void);
+
+#endif
