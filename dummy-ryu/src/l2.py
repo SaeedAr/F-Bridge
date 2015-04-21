03ed5eac1acdf33099e0d60f6d6eb90f0e145510
Parham Alvani <parham.alvani@gmial.com>
2015-04-22 00:39:51 +0430

    First test done with dummy-ryu.

--- dummy-ryu/src/l2.py
+++ dummy-ryu/src/l2.py
@@ -7,3 +7,10 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 __author__ = 'Parham Alvani'
+
+from ryu.base import app_manager
+
+
+class L2Switch(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+        app_manager.RyuApp.__init__(self, args, kwargs)
