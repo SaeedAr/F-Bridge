77dbfc038a2dc24011614d5e062e5787a8b652c0
Parham Alvani <parham.alvani@gmial.com>
2015-04-23 09:57:58 +0430

    gitignore updated.

--- T-REX/src/proxy.c
+++ T-REX/src/proxy.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 23-03-2015
  *
- * [] Last Modified : Thu 23 Apr 2015 09:51:35 AM IRDT
+ * [] Last Modified : Thu 23 Apr 2015 09:57:40 AM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -67,8 +67,8 @@ static void finish_msg(SoupSession *session, SoupMessage *msg2, gpointer data)
 }
 
 static void trex_callback(SoupServer *server, SoupMessage *msg,
-                          const char *path, GHashTable *query,
-                          SoupClientContext *client, gpointer user_data)
+		const char *path, GHashTable *query,
+		SoupClientContext *client, gpointer user_data)
 {
 	SoupMessage *msg2;
 	SoupSession *session;
@@ -80,28 +80,28 @@ static void trex_callback(SoupServer *server, SoupMessage *msg,
 	/* TODO Memory Leak :-) */
 	uristr = g_strjoin(NULL, DEST_URL, uristr, NULL);
 	g_print("[%p] %s %s HTTP/1.%d\n", msg, msg->method, uristr,
-	        soup_message_get_http_version(msg));
+			soup_message_get_http_version(msg));
 
 	msg2 = soup_message_new(msg->method, uristr);
 	soup_message_headers_foreach(msg->request_headers, copy_header,
-	                             msg2->request_headers);
+			msg2->request_headers);
 	soup_message_headers_remove(msg2->request_headers, "Host");
 	if (msg->request_body->length) {
 		SoupBuffer *request =
 			soup_message_body_flatten(msg->request_body);
 		soup_message_body_append_buffer(msg2->request_body,
-		                                request);
+				request);
 		soup_buffer_free(request);
 	}
 	soup_message_headers_set_encoding(msg->response_headers,
-	                                  SOUP_ENCODING_CHUNKED);
+			SOUP_ENCODING_CHUNKED);
 
 	soup_server_pause_message(server, msg);
 
 	g_signal_connect(msg2, "got_headers",
-	                 G_CALLBACK(recv_headers), msg);
+			G_CALLBACK(recv_headers), msg);
 	g_signal_connect(msg2, "got_chunk",
-	                 G_CALLBACK(recv_chunk), msg);
+			G_CALLBACK(recv_chunk), msg);
 
 	soup_session_queue_message(session, msg2, finish_msg, msg);
 
@@ -115,12 +115,12 @@ void trex_init(void)
 	GError *error = NULL;
 
 	server = soup_server_new(SOUP_SERVER_SERVER_HEADER,
-	                         "T-REZ-1.0", NULL);
+			"T-REZ-1.0", NULL);
 	soup_server_listen_all(server, 1373,
-	                       SOUP_SERVER_LISTEN_IPV4_ONLY, &error);
+			SOUP_SERVER_LISTEN_IPV4_ONLY, &error);
 	if (error) {
 		g_printerr("Unable to create server: %s\n",
-		           error->message);
+				error->message);
 		exit(1);
 	}
 	soup_server_add_handler(server, NULL, trex_callback, NULL, NULL);
