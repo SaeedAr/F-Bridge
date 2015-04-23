ef640717f23fa0a3ab165c00d4a8aec35b42a1df
Parham Alvani <parham.alvani@gmial.com>
2015-04-24 01:33:56 +0430

    Some test done on trap.c

--- TRAP/src/trap.c
+++ TRAP/src/trap.c
@@ -13,15 +13,20 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <libsoup/soup.h>
-#include <stdbool.h>
 
 #include "trap.h"
 
 static void trap_callback(SoupServer *server, SoupMessage *msg,
-		const char *path, GHashTable *query,
-		SoupClientContext *client, gpointer user_data)
+	const char *path, GHashTable *query,
+	SoupClientContext *client, gpointer user_data)
 {
 	g_print("%s\n", path);
+	SoupHTTPVersion version = soup_message_get_http_version(msg);
+	if (version == SOUP_HTTP_1_0)
+		g_print("1\n");
+	if (version == SOUP_HTTP_1_1)
+		g_print("1.1\n");
+	g_print("%s\n", soup_client_context_get_host(client));
 }
 
 void trap_init(void)
@@ -29,12 +34,12 @@ void trap_init(void)
 	GError *error = NULL;
 
 	SoupServer *server = soup_server_new(SOUP_SERVER_SERVER_HEADER,
-			"TRAP-1.0", NULL);
+		"TRAP-1.0", NULL);
 	soup_server_listen_all(server, 1373,
-			SOUP_SERVER_LISTEN_IPV4_ONLY, &error);
+		SOUP_SERVER_LISTEN_IPV4_ONLY, &error);
 	if (error) {
 		g_printerr("Unable to create server: %s\n",
-				error->message);
+			error->message);
 		exit(1);
 	}
 	soup_server_add_handler(server, NULL, trap_callback, NULL, NULL);
